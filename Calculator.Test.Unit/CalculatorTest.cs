f195b63f66ccd9320cc03a23cbe20a6dfc4cfc8e
Stine F. Gravesen <au553805@post.au.dk>
2020-02-05 11:19:34 +0100

    Added tests for multiply, subtract and add

--- Calculator.Test.Unit/CalculatorTest.cs
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -18,35 +18,68 @@ namespace Calculator.Test.Unit
             uut = new CalculatorExercise.Calculator();
         }
         [Test]
-        public void AddPositiveWithNegative()
+        public void Add_4andNegative2_Result2()
         {
-            //Arrange
-            var uut = new CalculatorExercise.Calculator();
 
             //Act + Assert
-            Assert.That(uut.Add(3, -5), Is.EqualTo(-2));
+            Assert.That(uut.Add(4, -2), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Add_Negative3andNegative12_ResultNegative15()
+        {
+
+            Assert.That(uut.Add(-3, -12), Is.EqualTo(-15));
+        }
+
+        [Test]
+        public void Add_8and22_Result30()
+        {
+
+            Assert.That(uut.Add(8, 22), Is.EqualTo(30));
         }
 
         [Test]
         public void Multiply_5and4_Result20()
         {
-            var uut = new CalculatorExercise.Calculator();
-
             Assert.That(uut.Multiply(5, 4), Is.EqualTo(20));
         }
 
         [Test]
         public void Multiply_5and0_Result0()
         {
-            var uut = new CalculatorExercise.Calculator();
 
             Assert.That(uut.Multiply(5, 0), Is.EqualTo(0));
         }
 
         [Test]
-        public void SubtractTest()
+        public void Multiply_Negative5andTwo_ResultNegative10()
+        {
+            Assert.That(uut.Multiply(-5, 2), Is.EqualTo(-10));
+        }
+
+        [Test]
+        public void Multiply_Negative6andNegative3_Result18()
+        {
+            Assert.That(uut.Multiply(-6, -3), Is.EqualTo(18));
+        }
+
+        [Test]
+        public void Subtract_10and7_Result3()
         {
             Assert.That(uut.Subtract(10,7), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Subtract_10andNegative7_Result3()
+        {
+            Assert.That(uut.Subtract(10, -7), Is.EqualTo(17));
+        }
+
+        [Test]
+        public void Subtract_Negative4andNegative5_Result1()
+        {
+            Assert.That(uut.Subtract(-4, -5), Is.EqualTo(1));
         }
 
         [Test]

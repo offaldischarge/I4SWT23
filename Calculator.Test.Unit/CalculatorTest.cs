4e793fa5f75ebc0938ffa48a6099dc4c0ea98ecf
Rene Street <20112809@post.au.dk>
2020-02-05 17:37:42 +0100

    Cleaned up in code, added comments for tests

--- Calculator.Test.Unit/CalculatorTest.cs
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -14,15 +14,17 @@ namespace Calculator.Test.Unit
     public class CalculatorTest
     {
         private CalculatorExercise.Calculator uut;
+
         [SetUp]
         public void Setup()
         {
             uut = new CalculatorExercise.Calculator();
         }
+
+        //Add method tests
         [Test]
         public void Add_4andNegative2_Result2()
         {
-
             //Act + Assert
             Assert.That(uut.Add(4, -2), Is.EqualTo(2));
         }
@@ -30,17 +32,16 @@ namespace Calculator.Test.Unit
         [Test]
         public void Add_Negative3andNegative12_ResultNegative15()
         {
-
             Assert.That(uut.Add(-3, -12), Is.EqualTo(-15));
         }
 
         [Test]
         public void Add_8and22_Result30()
         {
-
             Assert.That(uut.Add(8, 22), Is.EqualTo(30));
         }
 
+        //Multiply tests
         [Test]
         public void Multiply_5and4_Result20()
         {
@@ -50,8 +51,6 @@ namespace Calculator.Test.Unit
         [Test]
         public void Multiply_5and0_Result0()
         {
-            var uut = new CalculatorExercise.Calculator();
-
             Assert.That(uut.Multiply(5, 0), Is.EqualTo(0));
         }
 
@@ -67,6 +66,7 @@ namespace Calculator.Test.Unit
             Assert.That(uut.Multiply(-6, -3), Is.EqualTo(18));
         }
 
+        //Subtract tests
         [Test]
         public void Subtract_10and7_Result3()
         {
@@ -85,6 +85,8 @@ namespace Calculator.Test.Unit
             Assert.That(uut.Subtract(-4, -5), Is.EqualTo(1));
         }
 
+
+        //Division tests
         [Test]
         public void Division_10and2_Return5()
         {
@@ -109,12 +111,7 @@ namespace Calculator.Test.Unit
             Assert.That(() => uut.Divide(10, 0.0), Throws.TypeOf<System.DivideByZeroException>());
         }
 
-        [Test]
-        public void Accumulator_InitialValue_ReturnZero()
-        {
-            Assert.That(uut.Power(-10, 2), Is.EqualTo(100));
-        }
-
+        //Division tests
         [Test]
         public void Power_Raise10ToPowerOf2_Return100()
         {
@@ -133,28 +130,11 @@ namespace Calculator.Test.Unit
             Assert.That(uut.Power(-10, -4), Is.EqualTo(0.0001));
         }
 
-
+        //Accumulator tests
         [Test]
-        public void Clear_ClearAccumulatorAfterAdd_ReturnZero()
+        public void Accumulator_InitialValue_ReturnZero()
         {
-            uut.Add(3, 4);
-            uut.Clear();
-            Assert.That(uut.Accumulator, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void Clear_ClearAccumulatorAfterSubtract_ReturnZero()
-        {
-            uut.Subtract(2, 5);
-            uut.Clear();
-            Assert.That(uut.Accumulator, Is.EqualTo(0));
-        }
-
-        [Test]
-        public void Clear_ClearAccumulator_ReturnZero()
-        {
-            uut.Clear();
-            Assert.That(uut.Accumulator, Is.Zero);
+            Assert.That(uut.Power(-10, 2), Is.EqualTo(100));
         }
 
         [Test]
@@ -181,6 +161,30 @@ namespace Calculator.Test.Unit
             uut.Multiply(5, 5);
 
             Assert.That(uut.Accumulator, Is.EqualTo(25));
+        }
+
+        //Clear() tests
+        [Test]
+        public void Clear_ClearAccumulatorAfterAdd_ReturnZero()
+        {
+            uut.Add(3, 4);
+            uut.Clear();
+            Assert.That(uut.Accumulator, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Clear_ClearAccumulatorAfterSubtract_ReturnZero()
+        {
+            uut.Subtract(2, 5);
+            uut.Clear();
+            Assert.That(uut.Accumulator, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Clear_ClearAccumulator_ReturnZero()
+        {
+            uut.Clear();
+            Assert.That(uut.Accumulator, Is.Zero);
         }
     }
 }
